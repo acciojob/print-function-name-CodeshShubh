51cdab160b7f3f1f43cbe08adc9d023192b3ee5d
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2025-01-15 14:13:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function functionName(){
+	alert(arguments.callee.name);
+	
+}
+
+function AccioJob(){
+	alert(arguments.callee.name);
+}
+AccioJob(); 
+functionName();
